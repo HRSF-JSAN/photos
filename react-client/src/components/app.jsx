d948366f02434e91d0d36d1a6f7c644cf5df1452
mattman240 <mattmull98@gmail.com>
2018-03-05 17:13:20 -0800

    fixed render for proxy server

--- react-client/src/components/app.jsx
+++ react-client/src/components/app.jsx
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 101,
       pictures: ['https://s3-us-west-1.amazonaws.com/foodigo/italian2.jpg', 'https://s3-us-west-1.amazonaws.com/foodigo/italian5.jpg', 'https://s3-us-west-1.amazonaws.com/foodigo/italian+1.jpg'],
       comment: ['I have never had a bad experience when I am here', 'This is my go-to resturant', 'We come here 5 times a week it is so good'],
       name: ['Josh Alamillo', 'Nicholas Peyrichou', 'Mat Bagnall'],
@@ -14,7 +15,8 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getData(this.props.id);
+    this.state.id = this.props.id;
+    this.getData(this.state.id);
   }
 
   getData(resturant) {
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
+import Pictures from './components/app';
 
-// proxy will pass number in here
-ReactDOM.render(<App id={102} />, document.getElementById('app'));
+window.Pictures = Pictures;
